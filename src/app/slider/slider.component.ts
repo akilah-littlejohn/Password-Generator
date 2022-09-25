4d02edc6da996e2e1447091320220e2e12b7f046
wisdomkeeper <akilahlittlejohn@gmail.com>
2022-09-25 11:27:16 -0400

    1 file modified, 2 added (feat): add slider component from material

---
+++ src/app/slider/slider.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.css'],
+  imports:[MatSliderModule]
+
+})
+export class SliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
